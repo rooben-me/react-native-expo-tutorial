476ba5df62b084ec6f0f5d6657e8952895d99a64
rooben-me <rubankumar.nkl@gmail.com>
2023-05-22 23:10:18 +0530

    added image viewer and working image upload

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("Please select an image");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          selectedImage={selectedImage}
+          placeholderImageSource={PlaceholderImage}
+        />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button
+          onPress={pickImageAsync}
+          label="Choose a photo"
+          theme="primary"
+        />
+        <Button label="Use this photo" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    paddingTop: 58,
+    flex: 1,
+  },
+  footerContainer: {
+    padding: 48,
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
---
+++ components/ImageViewer.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Image } from "react-native";
+import React from "react";
+
+const ImageViewer = ({ placeholderImageSource, selectedImage }) => {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
+
+  return <Image style={styles.image} source={imageSource} />;
+};
+
+export default ImageViewer;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
